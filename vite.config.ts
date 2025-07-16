a62cc29f6b5f171767442a31b14ccc9b6d58c423
Raj Dhapse <rajdhapse@gmail.com>
2025-07-16 23:03:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,6 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      },
-      server: {
-          host: '0.0.0.0',
-          port: parseInt(process.env.PORT) || 5173,
-          allowedHosts: ['recommendation-9bxo.onrender.com','recommendation-teal.vercel.app']
-     }
+      }
     };
 });
